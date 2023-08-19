3e72c97bd3291e7d6d53ee887c205ecd3c9ac653
shyamsutar <shyamsutar629@gmail.com>
2023-08-19 16:58:03 +0530

    google authentication done

--- components/SignInButton.jsx
+++ components/SignInButton.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import {signIn} from "next-auth/react"
 
+
 export default function SignInButton(){
     return(
         <button onClick={() => signIn("google")} className="flex items-center gap-4 shadow-xl rounded-lg pl-3">
--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,5 +1,20 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import SignInButton from "./SignInButton";
 
+
 export default function UserInfo(){
-    return <SignInButton/>
+    const {status, data: session} = useSession();  // renames data to session
+
+    if(status == "authenticated"){
+        return <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-50">
+            <Image className="rounded-full " src={session?.user?.image} width={100} height={100} alt="image" />
+            <div>Name: <span className="font-bold">{session?.user?.name}</span></div>
+            <div>Email: <span className="font-bold">{session?.user?.email}</span></div>
+        </div>
+    }else{
+        return <SignInButton/>
+    }
 }
