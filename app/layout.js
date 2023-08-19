1ecf6a79d44e22a1d5e9ab2f0ed2e4b3c53e456b
shyamsutar <shyamsutar629@gmail.com>
2023-08-19 15:47:07 +0530

    created Google Provider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import { NextAuthProvider } from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <NextAuthProvider>
       <div className='max-w-3xl mx-auto'>
       <Navbar/>
       {children}
       </div>
+      </NextAuthProvider>
       </body>
     </html>
   )
--- components/SignInButton.jsx
+++ components/SignInButton.jsx
@@ -1,8 +1,11 @@
+"use client"
+
 import Image from "next/image";
+import {signIn} from "next-auth/react"
 
 export default function SignInButton(){
     return(
-        <button className="flex items-center gap-4 shadow-xl rounded-lg pl-3">
+        <button onClick={() => signIn("google")} className="flex items-center gap-4 shadow-xl rounded-lg pl-3">
             <Image src="/googleLogo.png" height={30} width={30} alt="google logo png" />
             <span className="bg-blue-500 text-white px-4 py-3 ">
                 
